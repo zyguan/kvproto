2be1d58b8b9bc5dc10b3b1e556af1f26365e9cf7
MyonKeminta <9948422+MyonKeminta@users.noreply.github.com>
2022-11-18 17:08:01 +0800

    Adjust pessimistic lock protocol for lock behavior optimizations (#872)

--- src/lib.rs
+++ src/lib.rs
@@ -187,11 +187,21 @@ pub mod cdc_adapt {
             fn fmt(&self, f: &mut ::std::fmt::Formatter) -> ::std::fmt::Result {
                 let mut buf = String::new();
                 match self {
-                    crate::cdcpb::Event_oneof_event::Entries(v) => ::protobuf::PbPrint::fmt(v, "Entries", &mut buf),
-                    crate::cdcpb::Event_oneof_event::Admin(v) => ::protobuf::PbPrint::fmt(v, "Admin", &mut buf),
-                    crate::cdcpb::Event_oneof_event::Error(v) => ::protobuf::PbPrint::fmt(v, "Error", &mut buf),
-                    crate::cdcpb::Event_oneof_event::ResolvedTs(v) => ::protobuf::PbPrint::fmt(v, "ResolvedTs", &mut buf),
-                    crate::cdcpb::Event_oneof_event::LongTxn(v) => ::protobuf::PbPrint::fmt(v, "Long", &mut buf),
+                    crate::cdcpb::Event_oneof_event::Entries(v) => {
+                        ::protobuf::PbPrint::fmt(v, "Entries", &mut buf)
+                    }
+                    crate::cdcpb::Event_oneof_event::Admin(v) => {
+                        ::protobuf::PbPrint::fmt(v, "Admin", &mut buf)
+                    }
+                    crate::cdcpb::Event_oneof_event::Error(v) => {
+                        ::protobuf::PbPrint::fmt(v, "Error", &mut buf)
+                    }
+                    crate::cdcpb::Event_oneof_event::ResolvedTs(v) => {
+                        ::protobuf::PbPrint::fmt(v, "ResolvedTs", &mut buf)
+                    }
+                    crate::cdcpb::Event_oneof_event::LongTxn(v) => {
+                        ::protobuf::PbPrint::fmt(v, "Long", &mut buf)
+                    }
                 }
                 write!(f, "{}", buf)
             }
@@ -200,7 +210,9 @@ pub mod cdc_adapt {
         #[allow(dead_code)]
         fn assert_fmt_debug() {
             fn require_impl_debug<T: ::std::fmt::Debug>(_: T) {}
-            require_impl_debug(crate::cdcpb::Event_oneof_event::Entries(::std::default::Default::default()));
+            require_impl_debug(crate::cdcpb::Event_oneof_event::Entries(
+                ::std::default::Default::default(),
+            ));
             require_impl_debug(crate::cdcpb::ChangeDataEvent::default());
         }
     }
@@ -210,7 +222,9 @@ pub mod cdc_adapt {
         #[allow(dead_code)]
         fn assert_fmt_debug() {
             fn require_impl_debug<T: ::std::fmt::Debug>(_: T) {}
-            require_impl_debug(crate::cdcpb::event::Event::Entries(::std::default::Default::default()));
+            require_impl_debug(crate::cdcpb::event::Event::Entries(
+                ::std::default::Default::default(),
+            ));
             require_impl_debug(crate::cdcpb::ChangeDataEvent::default());
         }
     }
